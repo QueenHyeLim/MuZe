c7c76e373e30e503683861f164066101f8832d8a
rimriim <checw0528@naver.com>
2023-11-15 17:52:59 +0900

    회원기능 구현

--- MuZe/src/main/java/com/kh/muze/member/model/service/MemberService.java
+++ MuZe/src/main/java/com/kh/muze/member/model/service/MemberService.java
@@ -6,4 +6,6 @@ public interface MemberService {
 	
 	Member loginMember(Member m);
 
+	int insertMember(Member m);
+
 }
--- MuZe/src/main/java/com/kh/muze/member/model/service/MemberServiceImpl.java
+++ MuZe/src/main/java/com/kh/muze/member/model/service/MemberServiceImpl.java
@@ -22,4 +22,9 @@ public class MemberServiceImpl implements MemberService {
 		return memberDao.loginMember(sqlSession, m);
 	}
 
+	@Override
+	public int insertMember(Member m) {
+		return memberDao.insertMember(sqlSession, m);
+	}
+
 }
