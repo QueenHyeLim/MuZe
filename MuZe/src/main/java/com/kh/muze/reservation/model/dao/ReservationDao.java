4ce518181c610d588c7a16433aa1d186560364c1
QueenHyeLim <146050874+QueenHyeLim@users.noreply.github.com>
2023-11-15 15:34:45 +0900

    ReservationDao 생성

---
+++ MuZe/src/main/java/com/kh/muze/reservation/model/dao/ReservationDao.java
@@ -0,0 +1,8 @@
+package com.kh.muze.reservation.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReservationDao {
+
+}
--- MuZe/src/main/java/com/kh/muze/reservation/model/service/ReservationService.java
+++ MuZe/src/main/java/com/kh/muze/reservation/model/service/ReservationService.java
@@ -1,5 +1,10 @@
 package com.kh.muze.reservation.model.service;
 
+import com.kh.muze.show.model.vo.Show;
+
 public interface ReservationService {
+	
+	// 예매하기 버튼 클릭시 최신 뮤지컬 목록 띄우기
+	Show showLatest();
 
 }
