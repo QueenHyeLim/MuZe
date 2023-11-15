858cd5cbbf7c1092902623d0e169a2536c69e75e
EUNKYUNGYU <123463032+EUNKYUNGYU@users.noreply.github.com>
2023-11-15 15:18:05 +0900

    xml 파싱 성공

--- MuZe/src/main/java/com/kh/muze/theater/controller/TheaterController.java
+++ MuZe/src/main/java/com/kh/muze/theater/controller/TheaterController.java
@@ -3,26 +3,21 @@ package com.kh.muze.theater.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 
 import com.kh.muze.show.controller.ShowController;
 
@@ -90,9 +85,54 @@ public class TheaterController {
 		
 		//System.out.println(responseText);
 		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(url);
+		
+		doc.getDocumentElement().normalize();
+		System.out.println("Root element : " + doc.getDocumentElement().getNodeName());
+		
+		NodeList nList = doc.getElementsByTagName("db");
+		System.out.println("파싱할 리스트 수 : " + nList.getLength());
+		
+		for(int i = 0; i < nList.getLength(); i++) {
+			Node nNode = nList.item(i);
+			
+			if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element)nNode;
+				System.out.println("-------------------------------");
+				System.out.println("시설명 : " + getTagValue("fcltynm", eElement));
+				System.out.println("공연장 수 : " + getTagValue("mt13cnt", eElement));
+				System.out.println("개관연도 : " + getTagValue("opende", eElement));
+				System.out.println("객석 수 : " + getTagValue("seatscale", eElement));
+				System.out.println("전화번호 : " + getTagValue("telno", eElement));
+				System.out.println("홈페이지 : " + getTagValue("relateurl", eElement));
+				System.out.println("주소 : " + getTagValue("adres", eElement));
+				System.out.println("위도 : " + getTagValue("la", eElement));
+				System.out.println("경도 : " + getTagValue("lo", eElement));
+				
+				model.addAttribute("fcltynm", getTagValue("fcltynm", eElement));
+				model.addAttribute("mt13cnt", getTagValue("mt13cnt", eElement));
+				model.addAttribute("opende", getTagValue("opende", eElement));
+				model.addAttribute("seatscale", getTagValue("seatscale", eElement));
+				model.addAttribute("telno", getTagValue("telno", eElement));
+				model.addAttribute("relateurl", getTagValue("relateurl", eElement));
+				model.addAttribute("adres", getTagValue("adres", eElement));
+				model.addAttribute("la", getTagValue("la", eElement));
+				model.addAttribute("lo", getTagValue("lo", eElement));
+			}
+		}
 		
 		return "theater/theaterMapView";
 		
+	}
+
+	private static String getTagValue(String tag, Element eElement) {
+	    NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+	    Node nValue = (Node) nlList.item(0);
+	    if(nValue == null) 
+	        return null;
+	    return nValue.getNodeValue();
 	}
 	
 	
