292b3e20f447c014293966806b4c0bc6ea073c85
JIJI <145553076+ozoizme@users.noreply.github.com>
2023-11-17 16:34:31 +0900

    seat controller
    00

--- MuZe/src/main/java/com/kh/muze/seat/controller/SeatController.java
+++ MuZe/src/main/java/com/kh/muze/seat/controller/SeatController.java
@@ -14,13 +14,6 @@ public class SeatController {
 	
 	@Autowired
 	private SeatService seatService;
-<<<<<<< Updated upstream
-	
-
-	
-	@RequestMapping("disabled.st")
-=======
->>>>>>> Stashed changes
 
 	@ResponseBody
 	@RequestMapping(value="disabled.st", produces="application/json; charset=UTF-8")
