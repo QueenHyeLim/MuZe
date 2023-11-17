3aed112fc8a6528931deb98be7ae96ce6f6ed126
JIJI <145553076+ozoizme@users.noreply.github.com>
2023-11-17 16:21:40 +0900

    Merge branch 'main' of https://github.com/QueenHyeLim/MuZe
    fffff

--- MuZe/src/main/java/com/kh/muze/seat/controller/SeatController.java
+++ MuZe/src/main/java/com/kh/muze/seat/controller/SeatController.java
@@ -3,7 +3,10 @@ package com.kh.muze.seat.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.kh.muze.reservation.model.vo.Reservation;
 import com.kh.muze.seat.model.service.SeatService;
 
 @Controller
@@ -12,11 +15,26 @@ public class SeatController {
 	@Autowired
 	private SeatService seatService;
 	
+<<<<<<< Updated upstream
 	
 	@RequestMapping("disabled.st")
 	public String disabledSeat(String musId, String selectDate) {
 		
 		
 		seatService.disabledSeat()
+=======
+	@ResponseBody
+	@RequestMapping(value="disabled.st", produces="application/json; charset=UTF-8")
+	public String disabledSeat(String musId, String selectDate) {
+		System.out.println(musId);
+		System.out.println(selectDate);
+		
+		Reservation reservation = new Reservation();
+		reservation.setMusId(musId);
+		reservation.setSelectDate(selectDate);
+				
+		return new Gson().toJson(seatService.disabledSeat(reservation));
+
+>>>>>>> Stashed changes
 	}
 }
