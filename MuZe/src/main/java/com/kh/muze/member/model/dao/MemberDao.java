bd44b71b6c0d491c7c52db5b5a27a3f724447aeb
rimriim <checw0528@naver.com>
2023-11-20 11:05:38 +0900

    jhjhg

--- MuZe/src/main/java/com/kh/muze/member/model/dao/MemberDao.java
+++ MuZe/src/main/java/com/kh/muze/member/model/dao/MemberDao.java
@@ -16,4 +16,9 @@ public class MemberDao {
 		return sqlSession.insert("memberMapper.insertMember", m);
 	}
 
+	public int updateMember(SqlSessionTemplate sqlSession, Member m) {
+		return sqlSession.update("memberMapper.updateMember", m);
+	}
+	
+
 }
