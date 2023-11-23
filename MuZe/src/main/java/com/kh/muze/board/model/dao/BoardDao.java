203572923b37399217e4599a061b975f4746f904
EUNKYUNGYU <123463032+EUNKYUNGYU@users.noreply.github.com>
2023-11-23 17:08:50 +0900

    board

--- MuZe/src/main/java/com/kh/muze/board/model/dao/BoardDao.java
+++ MuZe/src/main/java/com/kh/muze/board/model/dao/BoardDao.java
@@ -1,5 +1,26 @@
 package com.kh.muze.board.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.muze.board.model.vo.Board;
+
+@Repository
 public class BoardDao {
+
+	public int selectFboardCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("boardMapper.selectFboardCount");
+	}
+
+	public ArrayList<Board> selectFboardList(SqlSessionTemplate sqlSession, RowBounds rowBounds) {
+		return (ArrayList)sqlSession.selectList("boardMapper.selectFboardList", null, rowBounds);
+	}
+
+	public int insertFboard(SqlSessionTemplate sqlSession, Board b) {
+		return sqlSession.insert("boardMapper.insertFboard", b);
+	}
 
 }
