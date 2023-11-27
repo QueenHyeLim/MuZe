df7683af72bc9bea351df83daded77c735591ffd
JIJI <145553076+ozoizme@users.noreply.github.com>
2023-11-27 16:23:52 +0900

    캘린더 완료
    마지막 예약 완료후 insert만 남음

--- MuZe/src/main/java/com/kh/muze/calendar/model/service/CalendarService.java
+++ MuZe/src/main/java/com/kh/muze/calendar/model/service/CalendarService.java
@@ -30,4 +30,6 @@ public interface CalendarService {
 
 	int deleteDiary(HashMap map);
 
+	int updateTransaction(Attachment att, Diary diary);
+
 }
--- MuZe/src/main/java/com/kh/muze/calendar/model/vo/Diary.java
+++ MuZe/src/main/java/com/kh/muze/calendar/model/vo/Diary.java
@@ -8,15 +8,16 @@ import lombok.ToString;
 @Getter @Setter @ToString @NoArgsConstructor
 public class Diary {
 	
-	private int diaryUser;           //USER_NO
-	private int diaryNo;          //DIARY_NO
-	private String diaryName;     //DIARY_NAME
-	private String diaryTitle;    //DIARY_TITLE
-	private String diaryContent;  //DIRAY_CONTENT
-	private String diaryStatus;   //DIARY_YN
-	private String attStatus;     //ATT_YN
-	private String diaryDate;     //DIARY_DATE
-	private String createDate;    //CREATE_DATE
+	private int diaryUser;        // USER_NO
+	private int diaryNo;          // DIARY_NO
+	private String diaryName;     // DIARY_NAME
+	private String diaryTitle;    // DIARY_TITLE
+	private String diaryContent;  // DIRAY_CONTENT
+	private String diaryStatus;   // DIARY_YN
+	private String attStatus;     // ATT_YN
+	private String diaryDate;     // DIARY_DATE
+	private String createDate;    // CREATE_DATE
 	private String modifiedName;  // TABLE에는 없음
+	private int attNo;         // ATT_NO
 
 }
