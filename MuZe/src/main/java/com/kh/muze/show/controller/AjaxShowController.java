88674e062853b0728d151cddd6b6614fd0c10de9
EUNKYUNGYU <123463032+EUNKYUNGYU@users.noreply.github.com>
2023-12-12 09:16:33 +0900

    수정

--- MuZe/src/main/java/com/kh/muze/show/controller/AjaxShowController.java
+++ MuZe/src/main/java/com/kh/muze/show/controller/AjaxShowController.java
@@ -6,16 +6,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 @RestController
 public class AjaxShowController {
@@ -23,15 +18,19 @@ public class AjaxShowController {
 	@GetMapping(value="slist.sh", produces="application/json; charset=UTF-8")
 	public String ajaxShowList(String shprfnm, String prfstate, Model model) throws Exception {
 		
-		String url = "http://www.kopis.or.kr/openApi/restful/pblprfr";
-		url += "?service=" + ShowController.SERVICEYKEY;
-
-		url += "&cpage=1";
-		url += "&rows=88000";
-		if(prfstate != "00") {
-			url += "&prfstate=" + prfstate;
-		}
-		url += "&shprfnm=" + URLEncoder.encode(shprfnm, "UTF-8");
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append("http://www.kopis.or.kr/openApi/restful/pblprfr")
+					 .append("?service=")
+					 .append(ShowController.SERVICEYKEY)
+					 .append("&cpage=1")
+					 .append("&rows=88000")
+					 .append("&shprfnm=")
+					 .append(URLEncoder.encode(shprfnm, "UTF-8"))
+					 .append("&prfstate=")
+					 .append(prfstate);
+		
+		String url = stringBuilder.toString();
 		
 		URL requestUrl = new URL(url);
 		HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
